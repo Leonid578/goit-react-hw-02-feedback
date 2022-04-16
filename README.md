fccbc7847bf4d5b562877dff1eea804d0d9a9110
Leonid578 <Leonid.Ivanov187@gmail.com>
2022-04-16 21:13:22 +0300

    upd

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# goit-react-hw-02-feedback
+**Read in other languages: [Русский](README.md), [Polska](README.pl.md).**
+
+# React homework template
+
+Этот проект был создан при помощи
+[Create React App](https://github.com/facebook/create-react-app). Для знакомства
+и настройки дополнительных возможностей
+[обратись к документации](https://facebook.github.io/create-react-app/docs/getting-started).
+
---
+++ src/components/App.jsx
@@ -0,0 +1,46 @@
+import {Component} from 'react';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
+
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0
+  };
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((total, el) => total + el, 0);
+  }
+
+  countPositiveFeedbackPercentage = () => (Math.round(this.state.good / this.countTotalFeedback() * 100));
+
+  onFeedbackHandle = (e) => {
+    const stateName = e.currentTarget.name;
+    this.setState(prev => ({ [stateName]: prev[stateName] + 1 }));
+  }
+  btns = () => Object.keys(this.state);
+  
+  render() {
+    const {good, neutral, bad} = this.state
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={this.btns()} onLeaveFeedback={this.onFeedbackHandle}/>
+        </Section>
+        
+        <Section title="Statistics">
+          {this.countTotalFeedback() ?
+            <Statistics good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback}
+              positivePercentage={this.countPositiveFeedbackPercentage} /> :
+              <Notification message="There is no feedback"/> }
+        </Section>
+      </div>
+    );
+  }
+}
